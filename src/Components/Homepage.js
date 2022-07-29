06037d54d9e06055560ed439ff6681e3a69bc268
himanshi-gupta <himanshigupta579@gmail.com>
2022-07-29 12:54:11 +0530

    final commit

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,44 +1,70 @@
-import React, { useState } from 'react'
-import population_data from '../Data'
-import './SearchStyle.css'
-
+import React, { useState, useEffect } from "react";
+import "./SearchStyle.css";
 
 function Homepage() {
-    const [search, setSearch] = useState('');
-     const [rendered,setRendered]=useState(population_data.data);
+  const [population_data, setPopulation_data] = useState(null);
+  const [search, setSearch] = useState("");
+  const [rendered, setRendered] = useState([]);
 
-    
-    const searchData=()=>{
-        if(search === '') {
-            setRendered(population_data);
-            } else { 
-            
-            let temp = population_data.data.filter(x => x.Year === search);
-            console.log(search);
-            setRendered(temp);
-     }
-            
+  useEffect(() => {
+    fetch("https://datausa.io/api/data?drilldowns=State&measures=Population")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPopulation_data(data);
+
+        const path = location.pathname.substring(1);
+        if (path !== "") {
+          setSearch(path);
+          let temp = data.data.filter((x) => x.Year === path);
+          setRendered(temp);
+        } else {
+          setRendered(data.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const searchData = () => {
+    if (search === "") {
+      setRendered(population_data.data);
+    } else {
+      let temp = population_data.data.filter((x) => x.Year === search);
+      setRendered(temp);
     }
+    history.replaceState({}, "", search);
+  };
   return (
     <>
-        <div className='search-bar-menue'>
-        <input value={search} onChange={e => setSearch(e.target.value)} type='text' />
-        <button type="Submit" onClick={searchData}>Submit</button>
-        </div>
-        {rendered.map((state,i) =>{
-            return (
-                <ul key={i}>
-                    <li>{state['ID State']}</li>
-                    <li>{state.State}</li>
-                    <li>{state['ID Year']}</li>
-                    <li>{state.Year}</li>
-                    <li>{state.Population}</li>
-                    <li>{state['Slug State']}</li>
-                </ul>
-            )
-        })}
+      <div className="search-bar-menue">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Enter Year"
+        />
+        <button type="Submit" onClick={searchData}>
+          Submit
+        </button>
+      </div>
+      <h2>Total Results = {rendered.length} </h2>
+      {rendered.map((state, i) => {
+        return (
+          <ul key={i}>
+            <li>{state["ID State"]}</li>
+            <li>{state.State}</li>
+            <li>{state["ID Year"]}</li>
+            <li>{state.Year}</li>
+            <li>{state.Population}</li>
+            <li>{state["Slug State"]}</li>
+          </ul>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
